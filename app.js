3640c228fc81a31d898bd5baa0e4f78c76b1b832
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-05-11 19:03:25 +0100

    creating...

--- app.js
+++ app.js
@@ -7,7 +7,28 @@ import taskRoutes from "./src/Routes/taskRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+
+    origin: [
+
+        "http://127.0.0.1:5500",
+
+        "http://localhost:5500",
+
+        "https://yourfrontend.vercel.app",
+
+        "https://yourfrontend.netlify.app"
+    ],
+
+    methods: [
+        "GET",
+        "POST",
+        "PUT",
+        "DELETE"
+    ],
+
+    credentials: true
+}));
 
 app.use(express.json());
 
