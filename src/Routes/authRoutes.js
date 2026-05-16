1d5f4c32dbff98e6e571d6c3c1857493bcfdbeb4
Nnoli Higgs <arinzeprogress01@gmail.com>
2026-05-16 03:57:40 +0100

    creating...

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,6 +1,8 @@
 import { registerUser , loginUser } from "../controllers/authControllers.js";
 import { resetPassword, changePassword, logoutUser } from "../controllers/authControllers.js";
 
+import { protect } from "../middleware/authMiddleware.js";
+
 
 import express from "express";
 
